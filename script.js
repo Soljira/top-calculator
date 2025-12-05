6e7a7b0f24afeb0178ebedb81d04c783a057f9df
Soljira <soljiragit@gmail.com>
2025-12-05 18:32:50 +0800

    feat: add framework for basic operations

---
+++ script.js
@@ -0,0 +1,28 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+// function divide(a, b) {
+//     let invalidInput = false;
+//     let quotient = 0;
+// }
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
